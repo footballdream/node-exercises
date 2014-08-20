bc5aaa6d466a130ce7ceea6a45eaee68de7182c9
0000-bigtree <0000.bigtree@gmail.com>
2014-08-20 20:05:55 +0800

    use mysql and simplest jade view

--- hello-express/app.js
+++ hello-express/app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var morgan  = require('morgan') // https://github.com/expressjs/morgan
+var path = require('path');
+var mysql = require('mysql');
 
 var app = express();
 app.use(morgan('combined'));
@@ -10,6 +12,25 @@ logger = morgan(function (req, res) {
 });
 app.use(logger);
 */
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  port     : 3366
+});
+connection.query('USE everyday_development');
+
+app.set('port',  process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/sentences', function(req, res){
+  connection.query('SELECT * FROM sentences', function(err, rows){
+    res.render('sentences', {sentences : rows});
+  });
+});
 
 // curl http://localhost:3000/hello.txt
 app.get('/hello.txt', function(req, res){
@@ -22,8 +43,6 @@ app.put('/', function(req, res) {
   res.send(JSON.stringify({ status:"success" }));
 });
 
-var port = process.env.PORT || 3000;
-
-var server = app.listen(3000, function() {
+var server = app.listen(app.get('port'), function() {
   console.log('Listening on port %d', server.address().port);
 });
