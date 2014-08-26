e83305528fedfbef75b0cb890a4457ed7b059e16
0000-bigtree <0000.bigtree@gmail.com>
2014-08-27 02:51:45 +0800

    实现一个resful api，目前感觉还是没有grape方便，性能有提升，待进一步学习评估

--- everyday-english/routes/api.js
+++ everyday-english/routes/api.js
@@ -6,13 +6,23 @@ var ver = '1.0.0';
 router.route('/' + ver + '/sentences')
   // get all sentences
   .get(function(req, res) {
+    var page = parseInt(req.query.page) || 1;
+    var perPage = parseInt(req.query.perPage) || 10;
+    var sortAttr = req.query.sortAttr || 'id';
+    var sortDirection = req.query.sortDirection || 'asc';
+    var sortStr = sortAttr + ' ' + sortDirection;
+    
     db.Sentence.findAndCountAll({
-     offset: 0,
-     limit: 2        
+     attributes: ['id', 'english', 'chinese'],
+     offset: (page - 1) * perPage,
+     limit: perPage,
+     order: sortStr
     }).success(function(sentences) {
+      res.contentType('application/json;charset=utf-8');
       res.json(sentences);
     });
   })
+
   .put(function(req, res) {
     res.json({a: 'a', b: 'b'});
   })
