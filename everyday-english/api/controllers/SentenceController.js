e0a16298a8ed4988ed605c34a5b491be2a43c3ca
0000-bigtree <0000.bigtree@gmail.com>
2014-08-28 16:20:27 +0800

    实现sentence的crud简单界面

--- everyday-english/api/controllers/SentenceController.js
+++ everyday-english/api/controllers/SentenceController.js
@@ -6,9 +6,87 @@
  */
 
 module.exports = {
+  
+  index: function (req, res) {
+    Sentence.find(function(err, sentences){
+      if (err) return res.send(err, 500);
+      res.view({
+          model: sentences
+      });
+    });
+  },
+    
+  'new': function(req, res) {
+    res.view();
+  },
 
+  create: function(req, res) {
+  	var params = _.extend(req.query || {}, req.params || {}, req.body || {});
+  	Sentence.create(params, function sentenceCreated (err, createdSentence) {
+  		if (err) return res.send(err, 500);
+  		res.redirect('/sentence/show/'+ createdSentence.id);
+  	});
+  },
+
+  show: function (req, res) {  	
+  	var id = req.param('id');
+  	if (!id) return res.send("No id specified.", 500);
+  	Sentence.findOne(id, function sentenceFound(err, sentence) {
+  		if (err) return res.sender(err, 500);
+  		if (!sentence) return res.send("Sentence " + id + " not found", 404);
+
+  		res.view({
+  			obj: sentence
+  		})
+  	});
+  },
+
+  edit: function (req, res) {
+    var id = req.param('id');    
+    if (!id) return res.send("No id specified.", 500);
+    Sentence.findOne(id, function sentenceFound (err, obj){
+      if (err) return res.send(err, 500);
+      if (!obj) return res.send("Sentence " + id + " not found", 404);
+      
+      res.view({
+  			sentence: obj
+      })
+    });
+  },
+  
+  update: function (req, res) {
+    var params = _.extend(req.query || {}, req.params || {}, req.body || {});
+    var id = params.id;    
+    if (!id) return res.send("No id specified.", 500);
+
+    Sentence.update(id, params, function sentenceUpdated(err, updatedSentence) {
+      if (err) {
+        res.redirect('/sentence/edit');
+      }
+      if(!updatedSentence) {
+        res.redirect('/sentence/edit');
+      }
+      res.redirect('/sentence/show/' + id);
+    });
+  },
+	
+	destroy: function (req, res) {
+		var id = req.param('id');
+		if (!id) return res.send("No id specified.",500);
+
+		Sentence.find(id, function foundUser(err, sentence) {
+			if (err) return res.send(err, 500);
+			if (!sentence) return res.send("No user with that id exists.", 404);
+
+			Sentence.destroy(id, function sentenceDestroyed(err) {
+				if (err) return res.send(err, 500);
+				return res.redirect('/sentence');
+			});			
+		})
+	},
+	
     // a CREATE action  
-    create: function(req, res, next) {
+    createx: function(req, res, next) {
         var params = req.params.all();
         Sentence.create(params, function(err, sentence) {
             if (err) return next(err);
@@ -56,7 +134,7 @@ module.exports = {
     },
 
     // an UPDATE action
-    update: function(req, res, next) {
+    updatex: function(req, res, next) {
         var criteria = {};
         criteria = _.merge({}, req.params.all(), req.body);
         var id = req.param('id');
@@ -71,7 +149,7 @@ module.exports = {
     },
 
     // a DESTROY action
-    destroy: function(req, res, next) {
+    destroyx: function(req, res, next) {
         var id = req.param('id');
         if (!id) {
             return res.badRequest('No id provided.');
