8b8d7960e1f7b5d0562642afbbc60e7634921e77
0000-bigtree <0000.bigtree@gmail.com>
2014-08-29 17:47:02 +0800

    new protocol analysis

---
+++ agent-utils/data-protocol/inernalUtils.js
@@ -0,0 +1,12 @@
+"use strict";
+
+exports.cppString2JsString = function(buffer, offset) {
+  var end;
+  for(var i = offset; i < buffer.length; i++) {
+    if (0x00 == buffer[i]) {
+      end = i;
+      break;
+    }
+  }
+  return buffer.toString('ascii', offset, end);
+}
