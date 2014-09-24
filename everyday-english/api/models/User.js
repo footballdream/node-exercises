96f54d9d83a4fd40890c8dc41973d1567a0c297b
-global <0000.bigtree@gmail.com>
2014-09-24 20:49:52 +0800

    begint to study passport

--- everyday-english/api/models/User.js
+++ everyday-english/api/models/User.js
@@ -5,10 +5,39 @@
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
 
+var bcrypt = require('bcrypt');
+
 module.exports = {
 
-  attributes: {
+attributes: {
+    name: {
+      type: 'string',
+      required: true,
+      unique: true
+    },
+    password: {
+      type: 'string',
+      required: true
+    },
+    toJSON: function() {
+      var obj = this.toObject();
+      delete obj.password;
+      return obj;
+    }
+  },
 
+  beforeCreate: function(user, cb) {
+    bcrypt.genSalt(10, function(err, salt) {
+      bcrypt.hash(user.password, salt, functio(err, hash) {
+        if (err) {
+          console.log(err);
+          cb(err);
+        }else{
+          user.password = hash;
+          cb(null, user);
+        }
+      });
+    });
   }
-};
 
+};
