0d9240e52e84da65f2738b9de6a54ae7cdd7b44f
0000-bigtree <0000.bigtree@gmail.com>
2014-09-27 04:33:16 +0800

    minor modification

--- everyday-english/assets/app/js/controllers/SigninController.js
+++ everyday-english/assets/app/js/controllers/SigninController.js
@@ -3,19 +3,25 @@ var module = angular.module('app.controllers');
 
 // Signin Controller
 module.controller('SigninController', ['$scope', '$location', 'SigninService',
-  function ($scope, $location, SigninService) {
-    $scope.session = {showMessage: false,
-        message: ''};
-    $scope.signin = function () {
-        console.log('userName=' + $scope.session.username + ", password=" + $scope.session.password)
-        SigninService.signin($scope.session.username, $scope.session.password).then(function (data) {
-            if (201 == data.returnCode) {
-                $location.path('/sentences')
-            } else {
-                console.log('signin failed, userName=' + $scope.session.username);
-                $scope.session.showMessage = true;
-                $scope.session.message = '用户名或密码错误。';
-            }
+  function($scope, $location, SigninService) {
+    $scope.session = {
+      showMessage: false,
+      message: ''
+    };
+    
+    $scope.signin = function() {
+      console.log('userName=' + $scope.session.username + ", password=" +
+        $scope.session.password)
+      SigninService.signin($scope.session.username, $scope.session.password).then(
+        function(data) {
+          if (201 == data.returnCode) {
+            $location.path('/sentences')
+          } else {
+            console.log('signin failed, userName=' + $scope.session.username);
+            $scope.session.showMessage = true;
+            $scope.session.message = '用户名或密码错误。';
+          }
         });
     };
-}]);
+  }
+]);
