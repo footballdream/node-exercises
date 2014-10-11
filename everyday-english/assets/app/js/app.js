b06fe9bd309eda604582e1b3fccf2359e3cbe3b7
0000.bigtree <0000.bigtree@gmail.com>
2014-10-12 01:17:40 +0800

    添加了一个简单的http拦截器实现

--- everyday-english/assets/app/js/app.js
+++ everyday-english/assets/app/js/app.js
@@ -112,3 +112,36 @@ module.run(['$rootScope', '$state', 'AuthService',
         }
       });
 }]);
+
+module.factory('SimpleHttpInterceptor', ['$q', function($q) {
+  var interceptor = {
+    // 成功的请求方法
+    'request': function(config) {
+      console.log('request interceptor');
+      return config; // 或者 $q.when(config);
+    },
+    // 响应成功
+    'response': function(response) {
+      console.log('response interceptor');
+      return response; // 或者 $q.when(config);
+    },
+    // 请求发生了错误，如果能从错误中恢复，可以返回一个新的请求或promise
+    'requestError': function(rejection) {
+      return response; // 或新的promise
+      // 或者，可以通过返回一个rejection来阻止下一步
+      // return $q.reject(rejection);
+    },
+    // 请求发生了错误，如果能从错误中恢复，可以返回一个新的响应或promise
+    'responseError': function(rejection) {
+      return rejection; // 或新的promise
+      // 或者，可以通过返回一个rejection来阻止下一步
+      // return $q.reject(rejection);
+    }
+  };
+  return interceptor;
+}]);
+
+module.config(['$httpProvider', function($httpProvider) {
+  $httpProvider.interceptors.push('SimpleHttpInterceptor');
+}]);
+
