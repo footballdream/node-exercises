c562693ad5531c13279aa6d193bf7d8b100400d7
0000-bigtree <0000.bigtree@gmail.com>
2014-10-14 01:35:53 +0800

    登录时显示等待框

--- everyday-english/assets/app/js/controllers/SigninController.js
+++ everyday-english/assets/app/js/controllers/SigninController.js
@@ -1,7 +1,7 @@
 'use strict';
 module = angular.module('app.controllers');
-module.controller('SigninController', ['$scope', '$state', 'AuthService',
-  function($scope, $state, AuthService) {
+module.controller('SigninController', ['$scope', '$state', 'AuthService', 'blockUI', '$timeout',
+  function($scope, $state, AuthService, blockUi, $timeout) {
     $scope.setSigninUi();
     $scope.signinStatus = {
       userName: '',
@@ -11,17 +11,21 @@ module.controller('SigninController', ['$scope', '$state', 'AuthService',
     }; 
     
     $scope.signin = function() {
+      blockUi.start('正在登录……'); 
       AuthService.signin($scope.signinStatus.userName, $scope.signinStatus.password)
       .then(function(data) {
-        var code = AuthService.getSigninCode();
-        if (code.SUCCESSFUL === data.code) {
-          $scope.$emit('UserSignined', data.userName);
-          $state.go('blackboard');
-        } else {
-          $scope.signinStatus.isShowingMessage = true;
-          $scope.signinStatus.message = '用户名或密码错误。';
-        }
+        $timeout(function() { 
+          blockUi.stop(); 
+          var code = AuthService.getSigninCode();
+          if (code.SUCCESSFUL === data.code) {
+            $scope.$emit('UserSignined', data.userName);
+            $state.go('blackboard');
+          } else {
+            $scope.signinStatus.isShowingMessage = true;
+            $scope.signinStatus.message = '用户名或密码错误。';
+          }             
+        }, 800);       
       });
-    };
+      };
   }
 ]);
